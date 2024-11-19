6752b58947e23a968c52f2bad4a6d95092f0dd70
AndresArias09 <leonardoandres.arias09@gmail.com>
2024-11-18 22:29:33 -0500

    Init

---
+++ js.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById('btnSi');
+
+btn.addEventListener("click",() =>{
+    document.querySelector('.corazon').classList.add('show');
+    document.querySelector('.beso').classList.add('show');
+});
